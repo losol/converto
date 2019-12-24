01a398ddf1b7a131df4a72ae73fde904c74ca298
Ole Kristian Losvik <losvik+github@gmail.com>
2019-12-24 19:27:04 +0100

    Refactored to using POST

--- plugins/html2pdf/controllers/Html2Pdf.js
+++ plugins/html2pdf/controllers/Html2Pdf.js
@@ -1,36 +1,55 @@
-'use strict';
+"use strict";
 
-const service = require('../services/Html2Pdf');
+const service = require("../services/Html2Pdf");
+const { parseMultipartData } = require("strapi-utils");
 
 function getOptions(ctx) {
-  let scale = undefined;
-  if (ctx.query.hasOwnProperty('scale')) {
-    scale = parseFloat(ctx.query.scale);
+  let scale;
+  if (ctx.request.body.scale) {
+    scale = parseFloat(ctx.request.body.scale);
     if (isNaN(scale) || scale < 0.1 || scale > 2) {
-      ctx.throw(400, 'Invalid value for scale query parameter given. Expected is number within 0.1...2 range.');
+      ctx.throw(
+        400,
+        "Invalid value for scale query parameter given. Expected is number within 0.1...2 range."
+      );
     }
   }
   return {
-    format: ctx.query.format || 'A4',
+    format: ctx.request.body.format || "A4",
     scale: scale || 1
   };
 }
 
 module.exports = {
 
-  url2pdf: async (ctx) => {
-    if (!ctx.query.url) {
-      ctx.throw(400, 'url query parameter is required');
+  html2pdf: async ctx => {
+    // Check that either url or html is posted
+    if (!ctx.request.body.url && !ctx.request.body.html) {
+      ctx.throw(400, "Either url or html is required");
     }
-    ctx.set('Content-Type', 'application/pdf');
-    ctx.body = await service.url2pdf(ctx.query.url, getOptions(ctx));
-  },
 
-  html2pdf: async (ctx) => {
-    if (!ctx.request.body) {
-      ctx.throw(400, 'Request should be made with Content-Type: text/plain header');
+    if (ctx.request.body.url && ctx.request.body.html) {
+      ctx.throw(
+        400,
+        "Both url and html were posted. Only one is allowed, please do not confuse me."
+      );
     }
-    ctx.set('Content-Type', 'application/pdf');
-    ctx.body = await service.html2pdf(ctx.request.body, getOptions(ctx));
+    if (!ctx.request.body) {
+      ctx.throw(
+        400,
+        "Request should be made with Content-Type: multipart/form-data"
+      );
+    }
+
+    // Set Content type to output
+    ctx.set("Content-Type", "application/pdf");
+
+    // Return pdf based on request type
+    if (ctx.request.body.html) {
+      ctx.body = await service.html2pdf(ctx.request.body.html, getOptions(ctx));
+    }
+    else {
+      ctx.body = await service.url2pdf(ctx.request.body.url, getOptions(ctx));
+    }
   }
 };
