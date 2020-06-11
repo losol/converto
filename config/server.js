d94e1cb3c13714b92af8febb3504a96925c0fcca
Ole Kristian Losvik <ole@losol.no>
2020-06-11 23:32:32 +0200

    Added dotenv

--- config/server.js
+++ config/server.js
@@ -1,4 +1,8 @@
+require("dotenv").config({
+  path: `.env`,
+});
+
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
 });
