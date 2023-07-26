0fc56f69593b7a0d08d59ba3734ac0f11311c775
Meinte Van't Kruis <meinte@gmail.com>
2023-07-26 14:23:01 +0200

    feat: completed ts typings and added unit tests for controller and service.

---
+++ server/controllers/schema.ts
@@ -0,0 +1,47 @@
+import { z } from "zod";
+import errorMessages from "./errorMessages";
+//taken from puppeteer's PaperFormat type, sadly type to schema is not a thing, therefore copying
+
+const paperFormats = z.enum([
+  "Letter",
+  "Legal",
+  "Tabloid",
+  "Ledger",
+  "A0",
+  "A1",
+  "A2",
+  "A3",
+  "A4",
+  "A5",
+  "A6",
+]);
+export default z
+  .object({
+    html: z.string().optional(),
+    url: z.string().optional(),
+    scale: z.number().default(1),
+
+    format: paperFormats.default("A4"),
+  })
+  .superRefine(({ html, url, scale }, ctx) => {
+    if (!html && !url) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: errorMessages.URL_HTML_REQUIRED,
+      });
+      return z.NEVER;
+    }
+    if (html && url) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: errorMessages.ONLY_URL_OR_HTML,
+      });
+      return z.NEVER;
+    }
+    if (isNaN(scale) || scale < 0.1 || scale > 2) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: errorMessages.SCALE_INVALID_VALUE,
+      });
+    }
+  });
--- server/services/htmlToPdfService.ts
+++ server/services/htmlToPdfService.ts
@@ -1,51 +1,68 @@
 import { Strapi } from "@strapi/strapi";
-
-const puppeteer = require("puppeteer");
+import puppeteer, { Browser, HTTPResponse, Page, PaperFormat } from "puppeteer";
 
 /**
  * @param {!function} pageInit
  * @param {!{format?: string, scale?: number}} options
  * @returns {Promise<*>}
  */
-const generatePdf = async (pageInit, options) => {
+const generatePdf = async (
+  pageInit: Function,
+  format: PaperFormat,
+  scale: number
+): Promise<Buffer> => {
   // Check if NoSandbox flag is set. For running this on Heroku, the
   // PUPPETEER_NOSANDBOX environment variable must be set to "true"
   const puppeteerArgs =
     process.env.PUPPETEER_NOSANDBOX === "true" ? ["--no-sandbox"] : [];
-  const browser = await puppeteer.launch({
+  const browser: Browser = await puppeteer.launch({
     headless: "new",
     args: puppeteerArgs,
   });
-  const page = await browser.newPage();
+  const page: Page = await browser.newPage();
   await pageInit(page);
   const buffer = await page.pdf({
-    format: options.format,
-    scale: options.scale,
+    format: format,
+    scale: scale,
   });
   await browser.close();
   return buffer;
 };
 
-export default ({ strapi }: { strapi: Strapi }) => ({
-  /**
-   * @param {string} url URL to render.
-   * @param {!{format?: string, scale?: number}=} options
-   * @returns {Promise<Buffer>}
-   */
-  url2pdf: async (url, options = {}) => {
-    return await generatePdf(async (page) => {
-      await page.goto(url, { waitUntil: "networkidle2" });
-    }, options);
-  },
+export interface HTMLToPDFService {
+  url2pdf(url: string, scale?: number, format?: PaperFormat): Promise<Buffer>;
+  html2pdf(html: string, scale?: number, format?: PaperFormat): Promise<Buffer>;
+}
 
-  /**
-   * @param {string} html
-   * @param {!{format?: string, scale?: number}=} options
-   * @returns {Promise<Buffer>}
-   */
-  html2pdf: async (html, options = {}) => {
-    return await generatePdf(async (page) => {
-      await page.setContent(html, { waitUntil: "networkidle2" });
-    }, options);
-  },
-});
+export default () =>
+  ({
+    /**
+     * @param {string} url URL to render.
+     * @param {!{format?: string, scale?: number}=} options
+     * @returns {Promise<Buffer>}
+     */
+    url2pdf: async (url: string, scale = 1, format = "A4"): Promise<Buffer> => {
+      return await generatePdf(
+        async (page: Page) => {
+          await page.goto(url, { waitUntil: "networkidle2" });
+        },
+        format,
+        scale
+      );
+    },
+
+    /**
+     * @param {string} html
+     * @param {!{format?: string, scale?: number}=} options
+     * @returns {Promise<Buffer>}
+     */
+    html2pdf: async (html, scale = 1, format = "A4"): Promise<Buffer> => {
+      return await generatePdf(
+        async (page: Page) => {
+          await page.setContent(html, { waitUntil: "networkidle2" });
+        },
+        format,
+        scale
+      );
+    },
+  } as HTMLToPDFService);
